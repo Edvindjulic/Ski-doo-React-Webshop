77f49569457da54caa33c33e1411c04b0251c48e
lysmac <calle@lysmask.se>
2023-03-08 13:41:34 +0100

    installed mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import * as React from "react";
+
+import { Button } from "@mui/material";
+
 function App() {
-  return <h1>Hello Students, begin here!</h1>;
+  return (
+    <div>
+      <h1>Hello Students, begin here!</h1>
+      <Button variant="contained">TEST</Button>
+      <Button variant="text">TEST</Button>
+    </div>
+  );
 }
 
 export default App;
