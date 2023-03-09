fa2805d130a2d052971164bcd91c9e0ff3c61eb7
lysmac <calle@lysmask.se>
2023-03-09 13:41:41 +0100

    2 more fake data added and some changes to the table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -6,18 +6,22 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { products } from "../data";
-import { Avatar } from "@mui/material";
+import { Avatar, TableFooter } from "@mui/material";
 
 export default function BasicTable() {
+  const totalPrice = products.reduce((acc, product) => acc + product.price, 0);
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Image</TableCell>
-            <TableCell align="right">Produkt</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Product</TableCell>
+            <TableCell align="right">Amount</TableCell>
+            <TableCell align="right">
+              Summa: {totalPrice.toLocaleString("sv-SE")} SEK
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -28,14 +32,17 @@ export default function BasicTable() {
             >
               <TableCell component="th" scope="row">
                 <Avatar
-                  alt="Remy Sharp"
+                  alt={product.title}
                   src={product.image}
                   sx={{ width: 150, height: 150 }}
+                  variant="square"
                 />
               </TableCell>
               <TableCell align="right">{product.title}</TableCell>
-              <TableCell align="right">{product.description}</TableCell>
-              <TableCell align="right">{product.price}</TableCell>
+              <TableCell align="right">{product.id}</TableCell>
+              <TableCell align="right">
+                {product.price.toLocaleString("sv-SE")} SEK
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/data.ts
+++ src/data.ts
@@ -16,4 +16,22 @@ export const products: Product[] = [
       "Djup snö, hårda vinterförhållanden - det spelar ingen roll för Tundra LE. Med REV Gen4 plattformen kan föraren nu ta sig an allt med ultrasnabba köregenskaper, överlägsen bärförmåga och mångsidiga funktioner som alla drivs av pålitliga Rotax-motorer.",
     price: 10000,
   },
+  {
+    id: "2",
+    image:
+      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/trail/mxz/side/SKI-MY24-MXZ-Sport-600-EFI-Neo-Yellow-Studio-RSIDE-000BHRB00.png",
+    title: "MXZ Sport",
+    description:
+      "Dynamisk ledprestanda med värdeorienterade funktioner och förtroendeingivande körning. En skoter full av skoj utan att plånboken töms.",
+    price: 123900,
+  },
+  {
+    id: "3",
+    image:
+      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/deep-snow/freeride/spring-only/SKI-MY24-FREE-Standard-MOON-Neo-Mint-000VDRC00-Studio-RSIDE-NA-SpringOnly.png",
+    title: "Ski-Doo FreeRide",
+    description:
+      "Freeride klarar av att erövra extrema förhållanden i djupsnö och gör det med alldeles egen stil, och sticker ut i en värld där branta sluttningar och stora hopp är normen.",
+    price: 219900,
+  },
 ];
