bb7e957f8988d5e028329f919e7db354dc749299
lysmac <calle@lysmask.se>
2023-03-13 14:09:01 +0100

    removed outcommented code

--- src/pages/ConfirmOrder.tsx
+++ src/pages/ConfirmOrder.tsx
@@ -1,4 +1,3 @@
-import { generateId } from "../../data";
 import BasicTextFields from "../components/CheckoutForm";
 import BasicTable from "../components/table";
 import { useCart } from "../contexts/CartContext";
@@ -9,11 +8,6 @@ export default function ConfirmOrder() {
   return (
     <div>
       <div>Du har {cart.length} saker i kundvagnen</div>
-      {/* <div>
-        {cart.map((product) => (
-          <p key={generateId()}>{product.title}</p>
-        ))}
-      </div> */}
       <BasicTable />
       <BasicTextFields />
     </div>
