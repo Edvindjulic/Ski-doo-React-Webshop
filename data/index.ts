a83e2f4c75c59492b93a62163e55bfca3b706595
lysmac <calle@lysmask.se>
2023-03-20 12:31:12 +0100

    fixed so images are now hosted on github

--- data/index.ts
+++ data/index.ts
@@ -28,12 +28,10 @@ export const products: Product[] = [
     description:
       "Djup snö, hårda vinterförhållanden - det spelar ingen roll för Tundra LE. Med REV Gen4 plattformen kan föraren nu ta sig an allt med ultrasnabba köregenskaper, överlägsen bärförmåga och mångsidiga funktioner som alla drivs av pålitliga Rotax-motorer.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/sport-utility/tundra/side/SKI-MY24-TUN-LE-600-EFI-85Hp-Black-000GDRA00-Studio-RSIDE-NA.png",
-    background: 
-    "https://www.ski-doo.com/content/skidoo/sv_se/modeller/arbete/tundra/_jcr_content/root/modelteaser.coreimg.png/1676661010557/ski-my24-tun-le-600-efi-85hp-black-000gdra00-studio-34fr-na.png"
-    ,
+      "https://user-images.githubusercontent.com/77364209/226325839-3736baab-2f64-4bf9-adc4-00accf75408c.png",
+    background:
+      "https://user-images.githubusercontent.com/77364209/226326070-92da18c2-4f97-443d-b8ee-8321d8547109.png",
     price: 124900,
-
   },
   {
     id: "2",
@@ -41,13 +39,11 @@ export const products: Product[] = [
     description:
       "Dynamisk ledprestanda med värdeorienterade funktioner och förtroendeingivande körning. En skoter full av skoj utan att plånboken töms.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/trail/mxz/side/SKI-MY24-MXZ-X-RS-MOONT-Neo-Yellow-Studio-RSIDE-000UCRC00.png",
-    
-    background:
-    "https://www.ski-doo.com/content/skidoo/sv_se/modeller/led/mxz/_jcr_content/root/modelteaser.coreimg.png/1676660962486/ski-my24-mxz-x-rs-moont-neo-yellow-studio-34fr-000ucrc00-01.png"
-    ,
-    price: 123900,
+      "https://user-images.githubusercontent.com/77364209/226326216-9c31b63a-8542-4bbf-a35d-735409aa3f4a.png",
 
+    background:
+      "https://user-images.githubusercontent.com/77364209/226326208-bf3f7715-ad3b-4336-a2da-3bd806d5ce03.png",
+    price: 123900,
   },
   {
     id: "3",
@@ -55,12 +51,10 @@ export const products: Product[] = [
     description:
       "Freeride klarar av att erövra extrema förhållanden i djupsnö och gör det med alldeles egen stil, och sticker ut i en värld där branta sluttningar och stora hopp är normen.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/deep-snow/freeride/side/SKI-MY24-FREE-Standard-MOON-Neo-Mint-000VDRC00-Studio-RSIDE-NA.png",
-    background:"https://www.ski-doo.com/content/skidoo/sv_se/modeller/djup-sno/freeride/_jcr_content/root/modelteaser.coreimg.png/1676660948435/ski-my24-free-standard-moon-neo-mint-000vdrc00-studio-34fr-na.png"
-    ,
-    
+      "https://user-images.githubusercontent.com/77364209/226326358-36e0db32-aebc-4b0f-9214-67b4997bf8d9.png",
+    background:
+      "https://user-images.githubusercontent.com/77364209/226326355-83c18c3f-ef05-4888-962c-0cf95f40ac0b.png",
     price: 219900,
-
   },
   {
     id: "4",
@@ -68,25 +62,21 @@ export const products: Product[] = [
     description:
       "Framtagen för vinteräventyraren med längre drivband för bättre grepp och ökad komfort. Med två- och fyrtakts Rotax motorer finns det en Renegade för alla ledförhållanden.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/trail/renegade/side/SKI-MY24-REN-X-RS-900-ACE-Turbo-R-Hyper-Silver-Studio-RSIDE-000DBRC00.png",
+      "https://user-images.githubusercontent.com/77364209/226326683-6bdb2e97-d464-4751-8205-fc43de372b6b.png",
     background:
-    "https://www.ski-doo.com/content/skidoo/sv_se/modeller/led/renegade/_jcr_content/root/modelteaser.coreimg.png/1676660957454/ski-my24-ren-x-rs-900-ace-turbo-r-black-studio-34fr-000darj00.png"
-    ,
+      "https://user-images.githubusercontent.com/77364209/226326676-3856d6bd-f29b-445e-a4b5-ab160ba79a31.png",
     price: 125900,
-
   },
   {
     id: "5",
     title: "Ski-Doo Expedition",
     description:
-      "Ski-Doo Expedition-modellerna är otroligt mångsidiga och har en fantastisk kapacitet i en och samma skoter, både på och utanför leder. Jobba vid stugan ena timmen, starta ett nytt äventyr nästa timme."
-      ,
+      "Ski-Doo Expedition-modellerna är otroligt mångsidiga och har en fantastisk kapacitet i en och samma skoter, både på och utanför leder. Jobba vid stugan ena timmen, starta ett nytt äventyr nästa timme.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/crossover/expedition/side/SKI-MY24-EXP-SE-850-ETEC-Terra-Green-000AGRM00-Studio-RSIDE-NA.png",
-    background:"https://www.ski-doo.com/content/skidoo/sv_se/modeller/crossover/expedition/_jcr_content/root/modelteaser.coreimg.png/1676660984506/ski-my24-exp-le-900-ace-turbo-r-black-000awra00-studio-34fr-na.png"
-    ,
+      "https://user-images.githubusercontent.com/77364209/226326823-dbaea32b-a1f9-4cbe-ac39-5ba9bf73c3b0.png",
+    background:
+      "https://user-images.githubusercontent.com/77364209/226326819-0f57a078-0f5d-48dd-8d12-de53f8991446.png",
     price: 165900,
-
   },
   {
     id: "6",
@@ -94,11 +84,9 @@ export const products: Product[] = [
     description:
       "Brutal styrka möter förfinad kapacitet med Skandic. Dess enorma dragkapacitet och stora bäryta gör svåra arbeten enkla och dess stora bärförmåga gör att du kan köra dit du vill.",
     productImage:
-      "https://www.ski-doo.com/content/dam/global/en/ski-doo/my24/studio/sport-utility/skandic/side/SKI-MY24-SKA-LE-900-ACE-Black-000AHRA00-Studio-RSIDE-NA.png",
-    background:"https://www.ski-doo.com/content/skidoo/sv_se/modeller/arbete/skandic/_jcr_content/root/modelteaser.coreimg.png/1676661017564/ski-my24-ska-le-900-ace-black-000ahra00-studio-34fr-na.png"
-    ,
+      "https://user-images.githubusercontent.com/77364209/226326917-d77d4803-4aea-4794-9b86-360775dd217b.png",
+    background:
+      "https://user-images.githubusercontent.com/77364209/226326910-3b25b359-ab02-4d94-a541-02736800a1ac.png",
     price: 127900,
-
   },
-
 ];
