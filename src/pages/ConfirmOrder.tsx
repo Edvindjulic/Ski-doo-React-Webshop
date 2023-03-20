fbdc43f27af5bca7fcdf9a07d91e04b0d4e22369
lysmac <calle@lysmask.se>
2023-03-20 15:27:03 +0100

    added badge to header and data-cy to link and badge

--- src/pages/ConfirmOrder.tsx
+++ src/pages/ConfirmOrder.tsx
@@ -1,20 +1,9 @@
-import { IconButton } from "@mui/material";
-import { ShoppingCart as ShoppingCartIcon } from "@mui/icons-material";
-import StyledBadge from "@mui/material/Badge";
 import CheckoutForm from "../components/CheckoutForm";
 import BasicTable from "../components/table";
-import { useCart } from "../contexts/CartContext";
 
 export default function ConfirmOrder() {
-  const { cart } = useCart();
-
   return (
     <div>
-      <IconButton aria-label="cart">
-        <StyledBadge badgeContent={cart.length} color="warning">
-          <ShoppingCartIcon />
-        </StyledBadge>
-      </IconButton>
       <BasicTable />
       <CheckoutForm />
     </div>
