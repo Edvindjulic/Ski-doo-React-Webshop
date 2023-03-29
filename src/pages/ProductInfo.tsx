457f8239a6ede89f4497e059adf0ef26af4c7f08
lysmac <calle@lysmask.se>
2023-03-29 18:26:28 +0200

    fix so prodoctinfo works on newly added items

--- src/pages/ProductInfo.tsx
+++ src/pages/ProductInfo.tsx
@@ -1,18 +1,26 @@
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Avatar, Box, Button, Card, CardContent, Typography, useMediaQuery } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { CartItem, products } from "../../data";
+import { CartItem } from "../../data";
 import Snackbar from "../components/Snackbar";
 import { useCart } from "../contexts/CartContext";
-
+import { useProduct } from "../contexts/ProductContext";
 
 export default function ProductInfo() {
-
   const params = useParams();
 
   const { cart, setCart } = useCart();
   const { addProduct } = useCart();
+  const { product } = useProduct();
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [lastAddedProduct, setLastAddedProduct] = useState<
@@ -34,30 +42,58 @@ export default function ProductInfo() {
     setSnackbarOpen(false);
   };
 
-
-
   const backgroundImage =
     "https://www.ski-doo.com/content/dam/global/en/ski-doo/my22/images/models/Ski-Doo-Model-Essential-Background.jpg";
 
-  const selectedProduct = products.find((product) => product.id === params.id);
+  const selectedProduct = product.find((product) => product.id === params.id);
 
   const card = (
     <React.Fragment>
       <CardContent>
-        <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: "space-between", }}>
-        <Box sx={{display: 'flex', justifyContent: 'space-between', padding: '0.5rem 0.8rem' }} >
-          <Avatar src={selectedProduct?.image} alt="avatar" sx={{ width:'10rem', height: '5rem', padding: '0.5rem'}} />
-          <Box sx={{padding: '1.2rem'}} >
-            <Typography data-cy="product-title" variant="h4">{selectedProduct?.title}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "0.5rem 0.8rem",
+            }}
+          >
+            <Avatar
+              src={selectedProduct?.image}
+              alt="avatar"
+              sx={{ width: "10rem", height: "5rem", padding: "0.5rem" }}
+            />
+            <Box sx={{ padding: "1.2rem" }}>
+              <Typography data-cy="product-title" variant="h4">
+                {selectedProduct?.title}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-          <Box sx={{display: 'flex', justifyContent: 'space-between', padding:'0.5rem 1rem', fontSize: '60px' }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "0.5rem 1rem",
+              fontSize: "60px",
+            }}
+          >
             <Typography variant="h5">2024</Typography>
-            <Typography data-cy="product-price" variant="h5">{selectedProduct?.price}<span>SEK</span></Typography>
+            <Typography data-cy="product-price" variant="h5">
+              {selectedProduct?.price}
+              <span>SEK</span>
+            </Typography>
           </Box>
         </Box>
-        <Box sx={{padding: '0.8rem', display: 'flex', flexWrap: 'wrap'}}>
-          <Typography data-cy="product-description">{selectedProduct?.description}</Typography>
+        <Box sx={{ padding: "0.8rem", display: "flex", flexWrap: "wrap" }}>
+          <Typography data-cy="product-description">
+            {selectedProduct?.description}
+          </Typography>
         </Box>
       </CardContent>
     </React.Fragment>
@@ -67,7 +103,7 @@ export default function ProductInfo() {
     return <h1>Product not found</h1>;
   }
 
-  const matches = useMediaQuery('(min-width:1280px)')
+  const matches = useMediaQuery("(min-width:1280px)");
 
   return (
     <Box
@@ -75,45 +111,60 @@ export default function ProductInfo() {
         display: "flex",
         flexDirection: "column",
         backgroundImage: `url(${backgroundImage})`,
-        backgroundPositionY: '15%',
+        backgroundPositionY: "15%",
         backgroundRepeat: "no-repeat",
         height: "78.4vh",
         alignItems: "flex-end",
         justifyContent: "center",
-        padding: matches? '0.5rem' : '0rem'
+        padding: matches ? "0.5rem" : "0rem",
       }}
     >
       <Box
         sx={{
           display: "flex",
-          flexWrap: 'wrap',
+          flexWrap: "wrap",
           maxWidth: "40rem",
           flexDirection: "column",
           alignItems: "center",
-          
         }}
       >
-        <Card sx={{display: 'flex', flexDirection: 'column', zIndex: '10', height: matches? '22rem' : '26rem'}} variant="outlined">
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            zIndex: "10",
+            height: matches ? "22rem" : "26rem",
+          }}
+          variant="outlined"
+        >
           {card}
-          
-            <Box sx={{display: 'flex', justifyContent:'flex-end', marginLeft:'auto', width: '100%', paddingBottom: matches? 'auto' : '1rem' }}> 
-              <Button variant='contained' sx={{width: '4.5rem', margin: '0 0.8rem 0.8rem 0' }}
-                data-cy="product-buy-button"
-                onClick={() => {
-                  addProduct(selectedProduct as CartItem);
-                  setSnackbarOpen(true);
-                  setLastAddedProduct({
-                    title: selectedProduct.title,
-                    price: selectedProduct.price,
-                    image: selectedProduct.image,
-                  });
-                }}
-              >
 
-                <AddShoppingCartIcon />
-              </Button>
-            </Box>
-
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              marginLeft: "auto",
+              width: "100%",
+              paddingBottom: matches ? "auto" : "1rem",
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{ width: "4.5rem", margin: "0 0.8rem 0.8rem 0" }}
+              data-cy="product-buy-button"
+              onClick={() => {
+                addProduct(selectedProduct as CartItem);
+                setSnackbarOpen(true);
+                setLastAddedProduct({
+                  title: selectedProduct.title,
+                  price: selectedProduct.price,
+                  image: selectedProduct.image,
+                });
+              }}
+            >
+              <AddShoppingCartIcon />
+            </Button>
+          </Box>
         </Card>
         <Box
           sx={{
@@ -121,7 +172,9 @@ export default function ProductInfo() {
             left: "10%",
             width: "50%",
             height: "50%",
-            backgroundImage: matches? `url(${selectedProduct?.background})` : 'none',
+            backgroundImage: matches
+              ? `url(${selectedProduct?.background})`
+              : "none",
             backgroundSize: "contain",
             backgroundRepeat: "no-repeat",
             // zIndex: "200",
